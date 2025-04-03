73f47003e5ba00b5c36632bc94a3c7b880cce8a1
JerinXplore <jerinjerin101325@gmail.com>
2025-04-03 18:56:52 +0600

    added lift up state in arrays

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -3,16 +3,19 @@ import Category from "./Category/Category";
 import './categories.css'
 
 const Categories = ({categoriesPromise}) => {
-    const categories = use(categoriesPromise);
     const [orderedCategories,setOrderedCategories]=useState([]);
+    const categories = use(categoriesPromise);
+    
     const handleOrderedCategories = (category)=>{
-       console.log('clicked categories which are ordered',category)
+       console.log('clicked categories which are ordered',category);
+       const newOrderedCategories=[...orderedCategories,category];
+       setOrderedCategories(newOrderedCategories);
     }
     console.log(categories)
     return (
         <div>
            <h2>Available Foods:{categories.categories.length}</h2>
-           <h2>So far oredered:</h2>
+           <h2>So far oredered:{orderedCategories.length}</h2>
 
            <div className="categories">
            {
